de3e03d0755810667a67e8a0d04807d4a4d599da
Jhovane <jhovanesoto@gmail.com>
2025-04-08 10:01:03 -0600

    Fixed: Name of files are changed to a ID generated

--- ankiTools.py
+++ ankiTools.py
@@ -54,11 +54,16 @@ def validName(cad:str):
 
     path_string = cad
 
-    invalid_characters = r'[\\/:*?"<>|.]'
+    cad_size=100
 
-    cleaned_path = re.sub(invalid_characters, "_", path_string)
+    cleaned_path = "Yarnki_"
 
-    cleaned_path = cleaned_path.strip()
+    for n in path_string:
+        cleaned_path+=f"{ord(n):03}"
+
+    if len(cleaned_path)>cad_size:
+        cleaned_path=cleaned_path[:cad_size]
+    
     return cleaned_path
 
 def generateCards(phrase:str,cant:int,folder:str):
@@ -80,19 +85,24 @@ def generateCards(phrase:str,cant:int,folder:str):
         for num,n in enumerate(data):
             with Progress() as progress:
                 task=progress.add_task(f"Data:{n["transcription"]}",total=100)
+                cont=0
                 while(1):
                     try:
                         path=validName(n["transcription"])+".mp4"
                         progress.update(task,advance=33.33)
-
-                        downloadURLVideo(n["video"],folder+"/"+path)
+                        downloadURLVideo(n["video"],folder+"\\"+path)
                         progress.update(task,advance=33.33)
                         add_card("Yarnki",n["transcription"],path)
                         progress.update(task,advance=33.33)
                         
                         break
-                    except:
-                        progress.update(task,description="Error making card, please wait 5 seconds to try again...")
-
+                    except Exception as E:
+                        cont+=1
+                        print(path)
+                        progress.update(task,description=f"Error making card ({cont} times), {E}, please wait 5 seconds to try again...")
+                        if cont==10:
+                            progress.update(task,description="Error")
+                            print(f"After 10 attempts me can´t create {n["transcription"]} card")
+                            break
                         time.sleep(5)
             
